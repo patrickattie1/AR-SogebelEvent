75c9e41411f2355e553647d5350cbae8c1831b34
Patrick ATTIE <patrick.attie@esih.edu>
2019-08-16 07:44:40 -0500

    Commit #1 - August 16th, 2019 - Basic app - Augmentation image is in Assets\Datafiles\Photos directory

---
+++ Assets/Datafiles/Scripts/GameController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public void LoadMainScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
